58137a433de6cab6ec3b899ed5860114a4fc178a
SEan <hi.na3s@gmail.com>
2013-06-15 16:36:56 -0700

    Added sustains

--- sparky.ino
+++ sparky.ino
@@ -7,12 +7,17 @@
 #define SRCLK_Pin 2 //CLOCK
 #define NUM_REGISTERS 1 //how many registers are in the chain
 
+//This is the shift register library.
 Shifter shifter(SER_Pin, RCLK_Pin, SRCLK_Pin, NUM_REGISTERS); 
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+//This is the assigned IP of the arduino. 
 byte ip = IPAddress(192,168,1,111);
+
+//THIS IS THE RASPBERRY PI IP ADDRESS!
 byte server[] = { 192,168,1,100 };
 
 EthernetClient client;
@@ -54,8 +59,13 @@ int valvestatus[9] = {0,0,0,0,0,0,0,0,0};
 
 int currenteffect = 0;
 int currentframe = 0;
-int totaleffects = 5;
+int totaleffects = 6;
 int totalframes = 9;
+
+// int sustain = 0;
+// int sustains[9];
+
+//Count these effects and update 'totaleffects' everytime you add a new effect.
 int effects[totaleffects][totalframes][totalvalves] = {
 	{ 
 		//0 Tracer
@@ -147,15 +157,16 @@ void read(char c){
 
 void loop()
 {
-	
+	//The time. 
 	ti = millis();
 	sinceLast = ti - lastTrigger;
   
-  //if you are connected and data is available
+  //Take a byte
 	if (client.available()) {
 		c = client.read();	
 	}
-	  
+	 
+	//Connected? 
   if (!client.connected()) {
     Serial.println();
     Serial.println("disconnecting.");
@@ -165,34 +176,53 @@ void loop()
 	//Read incoming bytes.
 	read(c);
 	
-	//Iterate through effect frames
+	//The arduino has recieved 1-9
 	if(flame==1) {
+		
+		//Loop through the valves.
 		for(int v;v<totalvalves;v++){
-			// Serial.println("on");
+			
+			//Let's save the id of the torch for this particular frame in the current effect.
 			on = effect[currenteffect][currentframe][v];
-			if( sinceLast >= intval && on == '1') { 
+			
+			//If it has been at least X (see 'read') and this torch should be one.
+			// else if( sinceLast >= intval && on == '1' || sustains[v] > 0) { 
+			else if( sinceLast >= intval && on == '1') { 
 	      shifter.setPin(v, HIGH); shifter.write();
 				valvestatus[v] = '1';
+				lastTrigger = millis();
+				// if(sustain && sustains[v] > 0) {
+				// 				sustains[v] = sustains[v]-1;
+				// 			} else if(sustains[v] == 0 && sustain) {
+				// 				sustains[v] = sustain;
+				// 			}
+			//If the torch has been on for long enough or it is off (precautions)
 	     } else if ( (sinceLast >= fixedIntval && valvestatus[v] == '1') || on == '0') 
 			 { //the valve is on, but it's time to go off; 
 	      shifter.setPin(v, LOW); shifter.write(); 
 				valvestatus[v] = '0';
 	     }
 		}
-		lastTrigger = millis();
+		
 		currentframe++;
 		currentframe = (currentframe >= totalframes) ? 0 : currentframe;
-	}
-
-	if(flame==0) {
+		
+	} else {
+		
 		// Serial.println("off"); 
 		Serial.flush();
 		for(int v;v<totalvalves;v++) {
 			shifter.setPin(v, LOW); shifter.write(); 
 		}
+		
+		//Since it is off, next effect.
 		currenteffect++; 
+		
+		//Set back to zero if it has reached the last effect. 
 		currenteffect = (currenteffect < totaleffects) ? currenteffect : 0;
 	}
+	
+	//Flush serial, save memories. 
 	Serial.flush();
 
 }
