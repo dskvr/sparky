ad9b4018d0efdbdde9b39a91e40562e8756f7cfa
SEan <hi.na3s@gmail.com>
2013-06-15 17:31:14 -0700

    Bigger changes.

--- sparky.ino
+++ sparky.ino
@@ -23,6 +23,7 @@ byte server[] = { 192,168,1,100 };
 EthernetClient client;
 
 bool flame = 0;
+bool first = true;
 int intval = 100;
 
 void setup() {
@@ -59,7 +60,8 @@ int valvestatus[9] = {0,0,0,0,0,0,0,0,0};
 
 int currenteffect = 0;
 int currentframe = 0;
-int totaleffects = 6;
+
+int totaleffects = 8;
 int totalframes = 9;
 
 // int sustain = 0;
@@ -133,30 +135,54 @@ int effects[totaleffects][totalframes][totalvalves] = {
 		{0,0,1,0,0,0,0,0,0},
 		{0,0,0,1,0,0,0,0,0},
 		{0,0,0,0,0,0,0,0,1}
+	},
+	{ //6 L
+		{0,1,0,0,1,0,0,1,0},
+		{0,0,1,0,1,0,1,0,0},
+		{0,0,0,1,1,1,0,0,0},
+		{1,0,0,0,1,0,0,0,1},
+		{0,1,0,0,1,0,0,1,0},
+		{0,0,1,0,1,0,1,0,0},
+		{0,0,0,1,1,1,0,0,0},
+		{1,0,0,0,1,0,0,0,1},
+		{0,0,0,0,0,0,0,0,0}
+	},
+	{ //7
+		{0,0,1,1,0,0,0,1,0},
+		{0,1,0,1,0,0,0,0,1},
+		{0,0,0,0,0,1,0,1,1},
+		{0,0,0,1,0,0,1,1,0},
+		{0,1,0,0,0,1,1,0,0},
+		{1,0,0,0,0,1,0,1,0},
+		{1,1,0,1,0,0,0,0,0},
+		{0,1,1,0,0,1,0,0,0},
+		{0,0,1,1,0,0,0,1,0}
 	}
 } 
 
+//Have less than 9 frames in an effect;
+int effectFrameOffset[totaleffects] = {
+	0, //0
+	0, //1
+	0, //2
+	0, //3
+	0, //4
+	0, //5
+	-1,//6
+	0 //7
+}
+
 // char* sequences[5];
 
-void read(char c){
+void read(){
 	//Speed Settings
-	if(c) {
-		if(c == '0') {flame=0;}
-		if(c == '1') {flame=1; intval = 1111;}
-		if(c == '2') {flame=1; intval = 750;}
-		if(c == '3') {flame=1; intval = 580;}
-		if(c == '4') {flame=1; intval = 410;}
-		if(c == '5') {flame=1; intval = 300;}
-		if(c == '6') {flame=1; intval = 200;}
-		if(c == '7') {flame=1; intval = 140;}
-		if(c == '8') {flame=1; intval = 85;}
-		if(c == '9') {flame=1; intval = 30;}
-	}
+	
 	//effects
 }
 
 void loop()
 {
+	
 	//The time. 
 	ti = millis();
 	sinceLast = ti - lastTrigger;
@@ -174,7 +200,18 @@ void loop()
   }
 
 	//Read incoming bytes.
-	read(c);
+	if(c) {
+		if(c == '0') {flame=0;} //Flame off, iterates current effect.
+		if(c == '1') {flame=1; intval = 500;}
+		if(c == '2') {flame=1; intval = 450;}
+		if(c == '3') {flame=1; intval = 400;}
+		if(c == '4') {flame=1; intval = 350;}
+		if(c == '5') {flame=1; intval = 300;}
+		if(c == '6') {flame=1; intval = 250;}
+		if(c == '7') {flame=1; intval = 140;}
+		if(c == '8') {flame=1; intval = 85;}
+		if(c == '9') {flame=1; intval = 30;}
+	}
 	
 	//The arduino has recieved 1-9
 	if(flame==1) {
@@ -187,16 +224,16 @@ void loop()
 			
 			//If it has been at least X (see 'read') and this torch should be one.
 			// else if( sinceLast >= intval && on == '1' || sustains[v] > 0) { 
-			else if( sinceLast >= intval && state == '1') { 
+			else if( (sinceLast >= intval && state == '1') || (first && state == '1') ) { 
 	      shifter.setPin(v, HIGH); shifter.write();
 				valvestatus[v] = '1';
-				lastTrigger = millis();
 				// if(sustain && sustains[v] > 0) {
 				// 				sustains[v] = sustains[v]-1;
 				// 			} else if(sustains[v] == 0 && sustain) {
 				// 				sustains[v] = sustain;
 				// 			}
 			//If the torch has been on for long enough or it is off (precautions)
+				if(first) first = false;
 	     } else if ( (sinceLast >= fixedIntval && valvestatus[v] == '1') || state == '0') 
 			 { //the valve is on, but it's time to go off; 
 	      shifter.setPin(v, LOW); shifter.write(); 
@@ -204,15 +241,36 @@ void loop()
 	     }
 		}
 		
-		currentframe++;
-		currentframe = (currentframe >= totalframes) ? 0 : currentframe;
-		
+		//After all the valves have been set, we prepare the next frame.
+		//Let's do this outside of the loop so we don't call it unecessarily. 
+		if(sinceLast >= intval) {
+			
+			//A frame just triggered increment the frame
+			currentframe++;
+			
+			//Where is the boundary from the loop (applied to the end of the sequence)
+			int reset = (effectFrameOffset[currenteffect] < 0) ? 9+effectFrameOffset[currenteffect] : totalframes;
+			
+			//Normalize the keys
+			currentframe = (currentframe >= reset) ? 0 : currentframe;
+			
+			currentframe = ()  ? 
+			
+			//Set the last trigger afterwards. 
+			lastTrigger = millis();
+			
+		}
+
+	//The flame is OFF (client sent 0 or no client connected)
 	} else {
 		
 		// Serial.println("off"); 
 		Serial.flush();
+		
 		for(int v;v<totalvalves;v++) {
+			//All off.
 			shifter.setPin(v, LOW); shifter.write(); 
+			
 		}
 		
 		//Since it is off, next effect.
@@ -220,6 +278,7 @@ void loop()
 		
 		//Set back to zero if it has reached the last effect. 
 		currenteffect = (currenteffect < totaleffects) ? currenteffect : 0;
+		
 	}
 	
 	//Flush serial, save memories. 
